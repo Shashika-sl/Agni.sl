3ea775f4aece4f52bee483c4accf04e0ead784e5
Shashika-sl <sarathchandrasiri895@gmail.com>
2025-09-21 02:39:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " sjsnssjshhshrhrbbb", // ඔයාගෙ session id එක
-MONGODB: process.env.MONGODB || "mongodb://mongo:sjjkkd",  //ඔයාගෙ mongoDb url එක
+SESSION_ID: process.env.SESSION_ID || " Manu-MD&GE1xzZrQ#Xk4PCnzvTFSgjLFvvQGKwanGW6tCqkUDtUWLOHVR1QU", // ඔයාගෙ session id එක
+MONGODB: process.env.MONGODB || "mongodb://mongo:qdxquszCpjNbltPMCqPHrebbWeLzznTF@gondola.proxy.rlwy.net:57535",  //ඔයාගෙ mongoDb url එක
 };
  
